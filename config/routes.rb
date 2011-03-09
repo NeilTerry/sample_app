bb34a926c6a2973740e933ffad8c2dabc4e05503
Neil Terry <NeilTerry@gmail.com>
2011-03-09 00:53:11 -0500

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SampleApp::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/contact', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
+  root :to => 'pages#home'
+
+
   get "pages/home"
 
   get "pages/contact"
